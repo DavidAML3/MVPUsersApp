e44f5a9aa389ae1046d5df64f664ca6922c7c959
DavidAML3 <David.aml.3@hotmail.com>
2021-09-09 19:58:44 -0500

    added logic of MVP

--- MVPUsersApp/MVP/Views/UsersViewController.swift
+++ MVPUsersApp/MVP/Views/UsersViewController.swift
@@ -7,9 +7,68 @@
 
 import UIKit
 
-class UsersViewController: UIViewController {
+class UsersViewController: UIViewController, UITableViewDelegate {
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private var users = [User]()
+    
+    private let presenter = UserPresenter()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Users"
+        
+        // MARK: - TableView
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // MARK: -  Presenter
+        presenter.setViewDelegate(delegate: self)
+        presenter.getUsers()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
+    }
+}
+
+extension UsersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = users[indexPath.row].name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        // Ask presenter to handle the tap
+        presenter.didTap(user: users[indexPath.row])
+    }
+}
+
+extension UsersViewController: UserPresenterDelegate {
+    func presentUsers(users: [User]) {
+        self.users = users
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+//    func presentAlert(title: String, message: String) {
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//        present(alert, animated: true)
+//    }
 }
